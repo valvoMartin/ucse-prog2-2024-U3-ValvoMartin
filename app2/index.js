bdfbbb2f796ff9bead42ceb7be98732d4f4d5b01
MauroCominotti <maurocominotti@hotmail.com>
2023-05-19 19:37:30 -0300

    feat: agregar comparacion entre fechas con timezone

--- app2/index.js
+++ app2/index.js
@@ -1,22 +1,60 @@
-const express = require('express');
-const moment = require('moment-timezone');
-const axios = require('axios');
+const express = require("express");
+const moment = require("moment-timezone");
+const axios = require("axios");
 
 const app = express();
 const port = 4001;
 
-app.get('/checker', async (req, res) => {
-  const timezone = req.query.zone;
-  console.log(`Timezone for: ${timezone}`);
+// Algunas urls para testear:
+// - http://localhost:4001/checker
+// - http://localhost:4001/checker?zone=America/Argentina/Buenos_Aires
+// - http://localhost:4001/checker?zone=America/New_York
+// - http://localhost:4001/checker?zone=Europe/London
+// Mas acerca de timezones https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
-  const response = await axios.get(`http://localhost:4000/tz?zone=${timezone}`);
-  
-  console.log(`API call ${JSON.stringify(response.data)}`);
-  const data = response.data;
+app.get("/checker", async (req, res) => {
+  try {
+    const timezone = req?.query?.zone;
+    console.log(`Timezone for: ${timezone}`);
 
-  //pasar response.data.time a moment() y validar que la hora sea >= 9 y <= 18
+    if (!timezone)
+      return res.json({ resp: "No timezone specified in the request" });
 
-  res.json({ data });  
+    const response = await axios.get(
+      `http://localhost:4000/tz?zone=${timezone}`
+    );
+
+    console.log(`API call ${JSON.stringify(response.data)}`);
+    const data = response.data;
+
+    if (!data?.time)
+      return res.json({ resp: "Error getting the time from the /tz endpoint" });
+
+    let resp = null;
+    const dataTimeParsed = moment(data.time);
+
+    const fromHs = 8;
+    const toHs = 18;
+    // Seteando el tiempo a 8hs y 18hs
+    argentinaDateMorning = moment.tz(timezone);
+    argentinaDateMorning.hours(fromHs).minutes(0).seconds(0);
+    argentinaDateEvening = moment.tz(timezone);
+    argentinaDateEvening.hours(toHs).minutes(0).seconds(0);
+
+    // Pasamos response.data.time a moment() y validar que la hora sea >= 9 y <= 18
+    if (
+      dataTimeParsed.isAfter(argentinaDateMorning) &&
+      dataTimeParsed.isBefore(argentinaDateEvening)
+    ) {
+      resp = `The Time received is between ${fromHs}hs and ${toHs}hs ${timezone}: ${dataTimeParsed}`;
+    } else {
+      resp = `The Time received is NOT between ${fromHs}hs and ${toHs}hs ${timezone}: ${dataTimeParsed}`;
+    }
+
+    return res.json({ resp });
+  } catch (error) {
+    return res.json({ error: `Error processing the data: ${error}` });
+  }
 });
 
 app.listen(port, () => {
